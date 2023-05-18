91a790824f431b0da5414909cc92c55a4de65473
elvinrajkumar <elvin@arokee.com>
2023-05-18 18:24:46 +0530

    server

---
+++ mern_GQL/config.js
@@ -0,0 +1,4 @@
+export const MONGO_URI =
+  "mongodb+srv://elvin:uLRZzRshpBk5PYbi@nodetuts.lnjao.mongodb.net/mygqldB?retryWrites=true&w=majority";
+
+export const JWT_SECRET = "ssssssssssssssssssssssss";
